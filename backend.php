df2cd1ef3bf4a34653b3139a5d49a3a84e8ed22b
Vaseem Ahmed khan <vakhan93@gmail.com>
2013-03-24 15:42:10 +0530

    Update backend.php
    java class added in function defination

--- backend.php
+++ backend.php
@@ -6,8 +6,8 @@ $dim=$_POST['dim'];
 $var_name=$_POST['var_name'];
 $data_type=$_POST['data_type'];
 
-function make_options_C($var_name,$data_type,$dim,$no_args,$func_name){
-  $k=1;
+function make_options_C($var_name,$data_type,$dim,$no_args,$func_name){ //agruments for c returned in list_options
+	$k=1;
 	$list_options[0]="";
 	$list_options[0].=$func_name.'(';
 	for($i=0;$i<$no_args;++$i){
@@ -36,15 +36,18 @@ function make_options_C($var_name,$data_type,$dim,$no_args,$func_name){
 	}
 	for($j=0;$j<$k;++$j){
 		$list_options[$j]= substr($list_options[$j], 0, -1);
-		$list_options[$j].='){  }';
-		echo '<input type="radio" name="select" value="'.$list_options[$j].'">'.'<textarea name="comments" cols="95" rows="2">'.$list_options[$j].'
+		$list_options[$j].='){  
+		//write code here
+	}';
+		echo '<input type="radio" name="select" value="'.$list_options[$j].'">'.'<textarea name="comments" cols="95" rows="3">'.$list_options[$j].'
 		</textarea><br>';
 	}
 }
 
-function make_options_java($var_name,$data_type,$dim,$no_args,$func_name){
+function make_options_java($var_name,$data_type,$dim,$no_args,$func_name,$class_name){//agruments for JAVA returned in list_options
 	$k=1;
-	$list_options[0]="";
+	$list_options[0]="public ".$class_name."{
+	public ";
 	$list_options[0].=$func_name.'(';
 	for($i=0;$i<$no_args;++$i){
 
@@ -56,7 +59,6 @@ function make_options_java($var_name,$data_type,$dim,$no_args,$func_name){
 					$list_options[$j].=' ArrayList '.$var_name[$i].',' ;	
 			}
 			for($j=$k;$j<2*$k;++$j){
-					//if($data_type[$i]=='int')
 					$list_options[$j].=' Set<Integer> '.$var_name[$i].',' ;	
 			}
 			$k*=3;
@@ -77,8 +79,11 @@ function make_options_java($var_name,$data_type,$dim,$no_args,$func_name){
 
 	for($j=0;$j<$k;++$j){
 		$list_options[$j]= substr($list_options[$j], 0, -1);
-		$list_options[$j].='){  //write code here}';
-		echo '<input type="radio" name="select1" value="'.$list_options[$j].'">'.'<textarea name="comments" cols="95" rows="2">'.$list_options[$j].'
+		$list_options[$j].='){  
+		//write code here
+		}
+	}';
+		echo '<input type="radio" name="select1" value="'.$list_options[$j].'">'.'<textarea name="comments" cols="95" rows="5">'.$list_options[$j].'
 		</textarea><br>';
 	}
 }
@@ -86,7 +91,7 @@ echo '<html><body><form name="sendc" action="" method="">';
 echo '<h1>C function prototype</h1>';
 make_options_C($var_name,$data_type,$dim,$no_args,$func_name);
 echo '<h1>java function prototype</h1>';
-make_options_java($var_name,$data_type,$dim,$no_args,$func_name);
+make_options_java($var_name,$data_type,$dim,$no_args,$func_name,$class_name);
 echo '<input id="Submit2" type="submit" value="submit" /><input id="Reset2"  type="reset" value="reset" />';
 echo "</form></body></html>";
 
