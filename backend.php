07d34d29a8f451af527d5320895a67446cab29dc
Vaseem Ahmed khan <vakhan93@gmail.com>
2013-03-27 02:37:47 +0530

    choose a language for an input argument

--- backend.php
+++ backend.php
@@ -7,8 +7,7 @@ if(isset($_POST['dim']))
 	$dim=$_POST['dim'];
 	$var_name=$_POST['var_name'];
 	$data_type=$_POST['data_type'];
-	$list_optionsC[]=array();
-	$list_optionsJ[]=array();
+	
 	function make_options_C($var_name,$data_type,$dim,$no_args,$func_name,&$list_options){ //agruments for C returned in list_options
 		$config = parse_ini_file('configc.ini', true);
 		for($i=1;$i<=$no_args;++$i){
@@ -26,17 +25,17 @@ if(isset($_POST['dim']))
 	
 	
 	function printdata_c($list_option,$no_args){
-		echo '<select name="returnc">';
+		echo '<input type="text" name="returnc0"/> <select name="returnc"><option value="">----</option>';
 			for($j=0;$j<count($list_option[0]);++$j){
 			    echo '<option value="'.$list_option[0][$j].'">'.$list_option[0][$j].'</option>';
 			}
-		echo '</select>';
+		echo '</select><br/>';
 		for($i=1;$i<=$no_args;++$i){
-			echo '<select name="varc'.$i.'">';
+			echo '<input type="text" name="varc0'.$i.'"/> <select name="varc'.$i.'"><option value="">--</option>';
 			for($j=0;$j<count($list_option[$i]);++$j){
 			    echo '<option value="'.$list_option[$i][$j].'">'.$list_option[$i][$j].'</option>';
 			}
-			echo '</select>';
+			echo '</select><br/>';
 		}
 	} 
 	
@@ -55,19 +54,19 @@ if(isset($_POST['dim']))
 	}
 	
 	function printdata_java($list_option,$no_args){
-		echo '<select name="returnj">';
+		echo '<input type="text" name="returnj0"/> <select name="returnj"><option value="">----</option>';
 			for($j=0;$j<count($list_option[0]);++$j){
 			    echo '<option value="'.$list_option[0][$j].'">'.$list_option[0][$j].'</option>';
 			}
-			echo '</select>';
+		echo '</select><br/>';
 		for($i=1;$i<=$no_args;++$i){
-			echo '<select name="varj'.$i.'">';
+			echo '<input type="text" name="varj0'.$i.'"/> <select name="varj'.$i.'"><option value="">--</option>';
 			for($j=0;$j<count($list_option[$i]);++$j){
 			    echo '<option value="'.$list_option[$i][$j].'">'.$list_option[$i][$j].'</option>';
 			}
-			echo '</select>';
+			echo '</select><br/>';
 		}
-	} 
+	}
 
 	echo '<html><body><form id="form" action="backend.php" method="post">
 		<input type="hidden" name="no_args" value="'.$no_args.'"> <input type="hidden" name="func_name" value="'.$func_name.'"> 
@@ -91,24 +90,50 @@ if(isset($_POST['dim']))
 else{
 	if(isset($_POST['no_args']))
 	{
+		//print_r($_POST);
 		$no_args=$_POST['no_args'];
-		echo '<html><body><h1>C code</h1><textarea name="comments" cols="95" rows="4">'.$_POST["returnc"].' ( ';
-		for($j=1;$j<$no_args;++$j){
-				echo $_POST['varc'.$j].',';
+		
+		echo '<html><body><h1>C code</h1><textarea name="comments" cols="95" rows="4">';
+		
+		if(isset($_POST['returnc0'])&&$_POST['returnc0']!='')
+			echo $_POST["returnc0"].'( ';
+		else if(isset($_POST['returnc'])&&$_POST['returnc']!='')
+			echo $_POST["returnc"].'( ';
+			
+		if(isset($_POST['varc01'])&&$_POST['varc01']!="")
+			echo $_POST['varc01'];
+		else if(isset($_POST['varc1'])&&$_POST['varc1']!="")
+			echo $_POST['varc1'];
+		
+		for($j=2;$j<$no_args+1;++$j){
+				if(isset($_POST['varc0'.$j])&&$_POST['varc0'.$j]!="")
+					echo ','.$_POST['varc0'.$j];
+				else if(isset($_POST['varc'.$j])&&$_POST['varc'.$j]!="")
+					echo ','.$_POST['varc'.$j];
 		}
-		echo $_POST['varc'.$j].'){
+		echo '){
 		//write code here
-		}</textarea><br/>';
+	 }</textarea><br/>';
 		
 		echo'<h1>Java code</h1>';
 		echo '<textarea name="comments" cols="95" rows="6"> public class '.$_POST["class_name"].'{
-		public '.$_POST["returnj"].' ( ';
-			
-		for($j=1;$j<$no_args;++$j){
-				echo $_POST['varj'.$j].',';
-		}
+		';
 		
-		echo $_POST['varj'.$j].'){
+		if(isset($_POST['returnj0'])&&$_POST['returnj0']!='')
+			echo $_POST["returnj0"].'( ';
+		else if(isset($_POST['returnj'])&&$_POST['returnj']!='')
+			echo $_POST["returnj"].'( ';
+		if(isset($_POST['varj01'])&&$_POST['varj01']!="")
+			echo $_POST['varj01'];
+		else if(isset($_POST['varj1'])&&$_POST['varj1']!="")
+			echo $_POST['varj1'];
+		for($j=2;$j<$no_args+1;++$j){
+				if(isset($_POST['varj0'.$j])&&$_POST['varj0'.$j]!="")
+					echo ','.$_POST['varj0'.$j];
+				else if(isset($_POST['varj'.$j])&&$_POST['varj'.$j]!="")
+					echo ','.$_POST['varj'.$j];
+		}
+		echo '){
 			//write code here
 		}
 	}	</textarea><br/>	
